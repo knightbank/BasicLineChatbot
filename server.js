d6b53476bc3fb0942a8a253cd20b27381656a3be
knightbank <knightbank@gmail.com>
2018-01-05 23:29:20 +0700

    make it better

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ let handleMessageEvent = event => {
     else{
       msg = {
         type: 'text',
-        text: splitStr[0]+ " " +currencyList[splitStr[1]]
+        text: splitStr[0]+ " " +currencyList
       }
 //       switch(splitStr[0]){
 //         case "price" || "ราคา" :
