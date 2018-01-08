308f5175788f50c400804f442b33d298fccff700
knightbank <knightbank@gmail.com>
2018-01-08 14:11:14 +0700

    Deploy

---
+++ processCryptoCompareApi.js
@@ -0,0 +1,25 @@
+let request = require("request")
+let getJsonStr = require("./getJsonStr")
+
+require('bluebird');
+const jsonfile = require('jsonfile');
+let getCryptoCompareInfo
+let text
+
+getCryptoCompareInfo = (symbol) => {
+
+    return new Promise(function (resolve, reject) {
+        let coinId = symbol.toUpperCase();
+        getJsonStr("https://min-api.cryptocompare.com/data/pricemulti?fsyms="+coinId+"&tsyms=BTC,USD,THB")
+            .then((result) => {
+                JsonObj = result;
+          //if (err) return reject(err) // rejects the promise with `err` as the reason
+        resolve(JsonObj)               // fulfills the promise with `data` as the value
+            })
+      })          
+    }
+module.exports = getCryptoCompareInfo;
+//  getCryptoCompareInfo("btc")
+//  .then((text) => {
+//      console.log(text["BTC"]["USD"]);
+//     })
